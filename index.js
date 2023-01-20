66753fbf7ab35d716bdbbf0affb6977c5817a05e
Dan <35927536+dan-mba@users.noreply.github.com>
2023-01-20 09:05:47 -0500

    handle testimonials

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let exitCode = 0;
 
 if (fileNames.length > 0) {
   fileNames.forEach(file => {
-    const userPart = file.split("/")[1];
+    const splitFile = file.split("/");
+    const userPart = splitFile[1];
     if ((userPart !== user ) && (userPart !== `${user}.json`)){
+      if((splitFile.length == 4) && (splitFile[2] === "testimonials") && (splitFile[3] === `${user}.json`)) {
+        return;
+      }
       exitCode = 1;
       console.error("Filename does not mach user: ", file);
     }
